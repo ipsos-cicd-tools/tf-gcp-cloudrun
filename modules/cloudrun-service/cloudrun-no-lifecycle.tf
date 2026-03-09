8f9625df6ad286715ae7ddbfc065c3d07de04527
Mihai Topoleanu <mihai.topoleanu@ipsos.com>
2026-03-09 13:08:01 -0500

    add support for direct VPC connect and for disable IAM check

--- modules/cloudrun-service/cloudrun-no-lifecycle.tf
+++ modules/cloudrun-service/cloudrun-no-lifecycle.tf
@@ -127,7 +127,7 @@ resource "google_cloud_run_v2_service" "default_no_lc" {
     dynamic "volumes" {
       for_each = var.nfs_volumes != null ? var.nfs_volumes : {}
       content {
-        name = volumes.value.name
+                name = volumes.value.name
         nfs {
           server    = volumes.value.server # bug fix: was volumes.value.bucket
           path      = volumes.value.path
